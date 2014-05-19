9f103c2a90219dc40706681375bc807550b11585
OwenYang <usay@outlook.com>
2014-05-19 14:28:17 +0800

    refactor: refactor database connection with trible layer structure

---
+++ DataOperation/DAL/DataOperationDAL.cs
@@ -0,0 +1,113 @@
+﻿using DataOperation.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+
+namespace DataOperation.DAL
+{
+    public class DataOperationDAL
+    {
+        //把公共的代码，封装到一个方法中，提高代码的利用性
+        private static GradeField ToDataField(DataRow row)
+        {
+            GradeField gradeField = new GradeField();
+            gradeField.CompletedDate = (DateTime)row["CompletedDate"];
+            gradeField.Paragraph = (string)row["Paragraph"];
+            gradeField.Speed = (double)row["Speed"];
+            gradeField.BackSpace = (int)row["BackSpace"];
+            gradeField.HitKey = (double)row["HitKey"];
+            gradeField.KeyLong = (double)row["KeyLong"];
+            gradeField.WrongWords = (int)row["WrongWords"];
+            gradeField.WordsCount = (int)row["WordsCount"];
+            gradeField.KeyCount = (int)row["KeyCount"];
+            gradeField.CostTime = (string)row["CostTime"];
+
+            return gradeField;
+        }
+
+        //根据ID获得 GetById
+        public static GradeField GetById(long id)
+        {
+            DataTable dt =
+            SqlHelper.ExecuteDataTable("Select * from  T_Grade where Id=@Id",
+                new OleDbParameter("@Id", id));
+
+            if (dt.Rows.Count <= 0)
+                return null;
+            else if (dt.Rows.Count > 1)
+                throw new Exception("严重错误，查出多条数据");
+            else
+            {
+                DataRow row = dt.Rows[0];
+                return ToDataField(row);
+            }
+        }
+
+        public static void DeleteById(long id)
+        {
+            SqlHelper.ExecuteNonQuery("Delete from T_Grade where ID=@Id",
+                new OleDbParameter("@Id", id));
+        }
+
+        public static void Insert(GradeField gradeField)
+        {
+            SqlHelper.ExecuteNonQuery(@"Insert into T_Grade(
+                CompletedDate, Paragraph, Speed, BackSpace, HitKey, KeyLong, WrongWords, WordsCount, KeyCount, CostTime)
+                values(@CompletedDate, @Paragraph, @Speed, @BackSpace, @HitKey, @KeyLong, @WrongWords, @WordsCount, @KeyCount, @CostTime)",
+                new OleDbParameter("@CompletedDate", gradeField.CompletedDate.ToString("G")),
+                new OleDbParameter("@Paragraph", gradeField.Paragraph),
+                new OleDbParameter("@Speed", gradeField.Speed),
+                new OleDbParameter("@BackSpace", gradeField.BackSpace),
+                new OleDbParameter("@HitKey", gradeField.HitKey),
+                new OleDbParameter("@KeyLong", gradeField.KeyLong),
+                new OleDbParameter("@WrongWords", gradeField.WrongWords),
+                new OleDbParameter("@WordsCount", gradeField.WordsCount),
+                new OleDbParameter("@KeyCount", gradeField.KeyCount),
+                new OleDbParameter("@CostTime", gradeField.CostTime)
+                );
+        }
+
+        public static void Update(GradeField gradeField)
+        {
+            SqlHelper.ExecuteNonQuery(@"Update T_Grade set
+                CompletedDate=@CompletedDate,
+                Pagragraph=@Pagragraph,
+                Speed=@Speed,
+                BackSpace=@BackSpace,
+                HitKey=@HitKey,
+                KeyLong=@KeyLong,
+                WrongWords=@WrongWords,
+                WordsCount=@WordsCount,
+                KeyCount=@KeyCount,
+                CostTime=@CostTime",
+                new OleDbParameter("@CompletedDate", gradeField.CompletedDate.ToString("G")),
+                new OleDbParameter("@Pagragraph", gradeField.Paragraph),
+                new OleDbParameter("@Speed", gradeField.Speed),
+                new OleDbParameter("@BackSpace", gradeField.BackSpace),
+                new OleDbParameter("@HitKey", gradeField.HitKey),
+                new OleDbParameter("@KeyLong", gradeField.KeyLong),
+                new OleDbParameter("@WrongWords", gradeField.WrongWords),
+                new OleDbParameter("@WordsCount", gradeField.WordsCount),
+                new OleDbParameter("@KeyCount", gradeField.KeyCount),
+                new OleDbParameter("@CostTime", gradeField.CostTime)
+                );
+        }
+
+        public static List<GradeField> GetAll()
+        {
+            DataTable dt = SqlHelper.ExecuteDataTable("Select * from T_Grade");
+            List<GradeField> gradeFields = new List<GradeField>();
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow row = dt.Rows[i];
+                GradeField gradeField = new GradeField();
+                gradeField = ToDataField(row);
+                gradeFields.Add(gradeField);
+            }
+
+            return gradeFields;
+        }
+    }
+}
---
+++ DataOperation/DAL/SqlHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+
+namespace DataOperation.DAL
+{
+    class SqlHelper
+    {
+        //从App.Config读取数据库连接字符串
+        private static readonly string connStr =
+            ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+
+        //使用长度可变参数
+        public static int ExecuteNonQuery(string sql, params OleDbParameter[] parameters)
+        {
+            using (OleDbConnection conn = new OleDbConnection(connStr))
+            {
+                conn.Open();
+                using (OleDbCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        //执行查询，返回结果集中的第一行第一列
+        public static object ExecuteScalar(string sql, params OleDbParameter[] parameters)
+        {
+            using (OleDbConnection conn = new OleDbConnection(connStr))
+            {
+                conn.Open();
+                using (OleDbCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public static DataTable ExecuteDataTable(string sql, params OleDbParameter[] parameters)
+        {
+            using (OleDbConnection conn = new OleDbConnection(connStr))
+            {
+                conn.Open();
+                using (OleDbCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                    DataSet dataSet = new DataSet();
+                    adapter.Fill(dataSet);
+                    return dataSet.Tables[0];
+                }
+            }
+        }
+
+        #region 数据库DBNull与.NET的null转化
+        public static object FromDBValue(object value)
+        {
+            if (value == DBNull.Value)
+                return null;
+            else
+                return value;
+        }
+        public static object ToDbValue(object value)
+        {
+            if (value == null)
+                return DBNull.Value;
+            else
+                return value;
+        }
+
+        #endregion
+    }
+}
